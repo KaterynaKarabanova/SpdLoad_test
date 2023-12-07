c37b5b7ba825184214d644c612207bfef18f3efd
Kateryna <ikatya7575@gmaiil.com>
2023-12-07 22:58:35 +0300

    added basename at index.tsx

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/SpdLoad_test/">
       <RecoilRoot>
         <App />
       </RecoilRoot>
